81a70778209e243dd7f92cc0b3e1869e01062125
mveestor <47985910+mveestor@users.noreply.github.com>
2019-03-06 10:40:05 +0700

    Create pretest02.cpp

---
+++ pretest02.cpp
@@ -0,0 +1,31 @@
+/*
+Nama		: Jonathan Rafma Nanda Siregar
+NOM			: 140810180056
+Kelas		: B
+Tanggal		: 6 Maret 2019
+*/
+#include <iostream>
+using namespace std;
+void input(int (&arr)[100], int& n){}
+void MoveZeroToFront(int arr[], int n){}
+void output(int arr[], int n){}
+int main()
+{
+	int arr[100];
+	int n;
+	input(arr, n);
+	MoveZeroToFront(arr, n);
+	output(arr, n);
+}
+void input(int (&arr)[100], int& n)
+{
+	
+}
+void MoveZeroToFront(int arr[], int n)
+{
+	
+}
+void output(int arr[], int n)
+{
+	
+}
